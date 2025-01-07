25628f4f6860f2e9088cf7b9171ed902eff71ea0
fernanda castillo <vainillasenpai@gmail.com>
2025-01-07 12:45:30 -0600

    operaciones basicas

---
+++ operaciones basicas.js
@@ -0,0 +1,16 @@
+function suma(a,b){
+    return a + b;
+}
+console.log(suma(4, 10));
+function resta(c,d){
+    return c - d;
+}
+console.log(resta(4, 10));
+function multiplicacion(e,f){
+    return e * f;
+}
+console.log(multiplicacion(4, 10));
+function division(g,h){
+    return g / h;
+}
+console.log(division(5, 10));
