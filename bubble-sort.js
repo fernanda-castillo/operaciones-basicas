4b48387bb5741bee97126599db4548149969a67f
fernanda castillo <vainillasenpai@gmail.com>
2025-01-22 17:55:37 -0600

    se creo una secuencia para extraer y asignar numeros en orden

---
+++ bubble-sort.js
@@ -0,0 +1,124 @@
+const numeros = [42, 7, 15, 89, 3, 56, 12, 77, 30];
+
+let unaVariableTemporal = numeros[0]
+let otraVariabletemporal = numeros[1]
+numeros[0] = otraVariabletemporal
+numeros[1] = unaVariableTemporal
+
+console.log(numeros)
+
+unaVariableTemporal = numeros[1]
+otraVariabletemporal = numeros[2]
+numeros[1] = otraVariabletemporal
+numeros[2] = unaVariableTemporal
+
+console.log(numeros)
+
+unaVariableTemporal = numeros[3]
+otraVariabletemporal = numeros[4]
+numeros[3] = otraVariabletemporal
+numeros[4] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[3]
+otraVariabletemporal = numeros[2]
+numeros [3] = otraVariabletemporal
+numeros[2] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[2]
+otraVariabletemporal = numeros[1]
+numeros[2] = otraVariabletemporal
+numeros[1] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[1]
+otraVariabletemporal = numeros[0]
+numeros[1] = otraVariabletemporal
+numeros[0] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[5]
+otraVariabletemporal = numeros[4]
+numeros [5] = otraVariabletemporal
+numeros[4] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[6]
+otraVariabletemporal = numeros[5]
+numeros[6] = otraVariabletemporal
+numeros[5] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[5]
+otraVariabletemporal = numeros[4]
+numeros[5] = otraVariabletemporal
+numeros[4] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[4]
+otraVariabletemporal = numeros[3]
+numeros [4] = otraVariabletemporal
+numeros [3] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[3]
+otraVariabletemporal = numeros[2]
+numeros[3] = otraVariabletemporal
+numeros[2] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[7]
+otraVariabletemporal = numeros[6]
+numeros[7] = otraVariabletemporal
+numeros[6] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[8]
+otraVariabletemporal = numeros[7]
+numeros[8] = otraVariabletemporal
+numeros[7] = unaVariableTemporal
+
+console.log(numeros)
+
+
+unaVariableTemporal = numeros[7]
+otraVariabletemporal = numeros[6]
+numeros[7] = otraVariabletemporal
+numeros[6] = unaVariableTemporal
+
+console.log(numeros)
+
+unaVariableTemporal = numeros[6]
+otraVariabletemporal = numeros[5]
+numeros[6] = otraVariabletemporal
+numeros[5] = unaVariableTemporal
+
+console.log(numeros)
+
+unaVariableTemporal = numeros[5]
+otraVariabletemporal = numeros[4]
+numeros[5] = otraVariabletemporal
+numeros[4] = unaVariableTemporal
+
+console.log(numeros)
